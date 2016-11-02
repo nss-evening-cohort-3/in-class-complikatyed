9346f23f386652e3c714fbb2cb67bcc43aa6814f
complikatyed <katye.russell@gmail.com>
2016-11-02 18:48:48 -0500

    Parts four and five

--- BasicsOfForms/Controllers/BasicsController.cs
+++ BasicsOfForms/Controllers/BasicsController.cs
@@ -13,5 +13,29 @@ namespace BasicsOfForms.Controllers
         {
             return View();
         }
+
+        //POST:
+        [HttpPost]
+        public ActionResult Index(FormCollection form)
+        {
+            int i = 0;
+            return View();
+        }
+
+        // GET: Basics RAZOR
+        public ActionResult Razor()
+        {
+            return View();
+        }
+
+        //POST: Basics Razor
+        [HttpPost]
+        public ActionResult Razor(FormCollection form)
+        {
+            int i = 0;
+            return View();
+        }
+
+
     }
 }
