ea4a2848f459a36a0786cf8616905366302b25c8
complikatyed <katye.russell@gmail.com>
2016-11-02 20:58:26 -0500

    Add form controller

--- BasicsOfForms/Scripts/app/app.js
+++ BasicsOfForms/Scripts/app/app.js
@@ -1,1 +1,7 @@
-﻿var app = angular.module('basicsOfForms', []);
+﻿var app = angular.module("basicsOfForms", []);
+
+app.controller("FormController", ["$scope", function($scope) {
+    $scope.formSubmit = function () {
+
+    };
+}]);
