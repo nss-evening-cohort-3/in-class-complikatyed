3d7fec25e9cc688f30878693dbd9c7254ec2037b
complikatyed <katye.russell@gmail.com>
2016-11-02 21:32:49 -0500

    Adding stuff for getting the form data - currently not working

--- BasicsOfForms/Scripts/app/app.js
+++ BasicsOfForms/Scripts/app/app.js
@@ -1,7 +1,14 @@
-﻿var app = angular.module("basicsOfForms", []);
+﻿var app = angular.module('basicsOfForms', []);
 
-app.controller("FormController", ["$scope", function($scope) {
-    $scope.formSubmit = function () {
+app.controller("FormController", function ($scope) {
+
+    $scope.formSubmit = function (event) {
+
+        var form = $("#myform").toString();
+        console.log(form);
+
+        event.preventDefault();
+
 
     };
-}]);
+});
